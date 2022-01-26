c870552a8fccf8927d9e0dd227b320bf7553970a
delim001 <delime-codrinkevin@hotmail.fr>
2022-01-26 09:40:06 +0100

    mis a jour du front

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
 /**
  * @Route("/account")
@@ -12,12 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccountController extends AbstractController
 {
     /**
- * @Route("/", name="account")
- */
+     * @Route("/account", name="account")
+     */
     public function index(): Response
     {
+        $user = $this->repository->findAll();
+
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'controller_name' => $user,
         ]);
     }
 }
